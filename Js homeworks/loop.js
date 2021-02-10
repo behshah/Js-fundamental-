8929de3a826af41c925afff7e1b6e4f277237b8f
behshah <66421973+behshah@users.noreply.github.com>
2021-02-10 16:31:24 +0700

    Js updated

--- Js homeworks/loop.js
+++ Js homeworks/loop.js
@@ -2,7 +2,7 @@ var person = [ 'hasav','mamad', 'mortez', 'ali', 'aiereen' , 'behnam',
  'pasargad','walter', 'basir'];
 var numName = person.length;
 
-for (var i = 0 ; i<numName ; i++) {
+for (var i = numName-1 ; i>=0 ; i--) {
 
     console.log(person [i])
 
