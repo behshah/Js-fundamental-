e6cced3e4c10713366e4cb4656428d7563246da5
behshah <66421973+behshah@users.noreply.github.com>
2021-02-11 12:56:54 +0700

    function parameters

---
+++ Js homeworks/functionparameters.js
@@ -0,0 +1,46 @@
+var beer_cost = 6.75;
+var burgur_cost = 5.00;
+var pop_cost = 3.00;
+
+var account_blacalce = 15.75;
+//Drink beer
+function drink_beer(){
+    account_blacalce = account_blacalce - beer_cost;
+    console.log(account_blacalce);
+}
+// Eat burger
+function eat_burgur (){ 
+    account_blacalce = account_blacalce - burgur_cost;
+    console.log(account_blacalce);
+    }
+ // Drink pop
+function drink_pop (){
+ account_blacalce = account_blacalce - pop_cost;
+ console.log(account_blacalce);
+}
+
+// Visit carnaval , Drink and eat
+
+var mealList = [drink_beer, eat_burgur, drink_pop,drink_beer]
+function visitCarnaval (mList){
+for(var i = 0 ,  num = mList.length ; i<num ; i++ ) {
+// mList[i]();
+    mList[i].call();
+};
+
+}
+visitCarnaval(mealList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
